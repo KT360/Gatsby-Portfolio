7b8273dcd7fdfabf735e734520c6bc7cb1ff6ab3
Astrid <kamtoastrid360@gmail.com>
2022-07-06 21:14:10 -0400

    new pfp

--- src/@lekoarts/gatsby-theme-cara/sections/ProfilePicture.js
+++ src/@lekoarts/gatsby-theme-cara/sections/ProfilePicture.js
@@ -15,7 +15,7 @@ function ProfilePicture()
 {
     return(
         <div style={p_style} >
-            <img src={'banner.jpg'} style={{width:220, height:220, borderRadius:110}} alt="test"/>
+            <img src={'favicon.png'} style={{width:220, height:220, borderRadius:110}} alt="test"/>
         </div>
     );
 }
